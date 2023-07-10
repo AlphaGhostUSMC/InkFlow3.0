f9dfd65e66c76b25491ebbae2b6c11e720882dce
AlphaGhostUSMC <Soulofninja69@gmail.com>
2023-07-10 01:23:48 -0700

    Login Card Design

--- client/src/assets/js/bs-init.js
+++ client/src/assets/js/bs-init.js
@@ -1,9 +1,7 @@
-import * as bootstrap from 'bootstrap';
+document.addEventListener('DOMContentLoaded', function() {
 
-document.addEventListener('DOMContentLoaded', function () {
-
-	let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bss-tooltip]'));
-	tooltipTriggerList.map(function (tooltipTriggerEl) {
-		return new bootstrap.Tooltip(tooltipTriggerEl);
+	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bss-tooltip]'));
+	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+	  return new bootstrap.Tooltip(tooltipTriggerEl);
 	})
 }, false);
